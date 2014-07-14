1d6f3d47af7c6ca38d4c1e9b2f0377c62259aa50
zaphod1984 <zaphod84@gmx.de>
2014-07-14 10:12:58 +0200

    stripped out frontend specific stuff

--- frontendSource/script/app.js
+++
@@ -1,8 +0,0 @@
-var foo = require('./lib/foo');
-var angular = require('./lib/angular');
-var $ = require('./lib/jquery');
-
-console.log( 'welcome, the app has been bootstrapped: ' + foo() );
-
-console.log( 'i present you angular: ', angular);
-console.log( 'i present you jquery: ', $ );
--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-
-var configuration = require('./lib/configuration');
-
-express()
-    .use(express.static(__dirname + '/frontend'))
-    .listen(configuration.port);
-
-console.log( 'running on: ' + configuration.port );
