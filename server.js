4af5b8a718f595dc7d60f84d89aa6fe947e91b35
zaphod1984 <zaphod84@gmx.de>
2014-07-29 09:31:45 +0200

    cors added
    using the cors middleware to allow for cross origin requests

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var configuration = require('./lib/configuration');
 var bootstrap = require('./lib/bootstrap');
 
 var app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
-
-app.options('*', function(req, res, next) {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
-    res.set('Access-Control-Allow-Headers', 'Content-Type');
-    res.status(200).end();
-});
 
 bootstrap.bootstrap(app, configuration, function() {
     app.listen(configuration.port);
