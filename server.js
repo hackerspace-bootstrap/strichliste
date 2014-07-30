913c45c010197192c32eca1db6024a3ceecdcb4a
zaphod1984 <zaphod84@gmx.de>
2014-07-30 14:31:41 +0200

    added a little stupid splash screen

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+var figlet = require('figlet');
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
 var configuration = require('./lib/configuration');
+var version = require('./lib/version');
 var bootstrap = require('./lib/bootstrap');
 
 var app = express();
@@ -13,5 +15,11 @@ app.use(bodyParser.json());
 bootstrap.bootstrap(app, configuration, function() {
     app.listen(configuration.port);
 
-    console.log('running on port ' + configuration.port);
+    figlet('Strichliste v' + version, function(error, data) {
+        if (!error) {
+            console.log(data);
+        }
+
+        console.log('running on port ' + configuration.port);
+    });
 });
