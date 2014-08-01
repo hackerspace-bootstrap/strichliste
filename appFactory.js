e0df31a34fbfc009f2392e7fe36629bd288d8196
zaphod1984 <zaphod84@gmx.de>
2014-08-01 13:24:24 +0200

    refactoring
    * seperated server.js
      * app.js does the app bootstrapping
      * server.js does the server bootstrapping
    * seperation is needed for integration tests

---
+++ appFactory.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var configuration = require('./lib/configuration');
+var version = require('./lib/version');
+var bootstrap = require('./lib/bootstrap');
+
+module.exports.create = function(callback) {
+    var app = express();
+
+    app.use(cors());
+    app.use(bodyParser.json());
+
+    bootstrap.bootstrap(app, configuration, function(error) {
+        if (error) return callback(error);
+
+        callback(null, app);
+    });
+};
--- server.js
+++ server.js
@@ -1,24 +1,16 @@
 var figlet = require('figlet');
-var express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
 
-var configuration = require('./lib/configuration');
 var version = require('./lib/version');
-var bootstrap = require('./lib/bootstrap');
+var configuration = require('./lib/configuration');
+var appFactory = require('./appFactory');
 
-var app = express();
+appFactory.create(function(error, app) {
+    if (error) return console.log('could not launch the server: ' + error.message);
 
-app.use(cors());
-app.use(bodyParser.json());
-
-bootstrap.bootstrap(app, configuration, function() {
     app.listen(configuration.port);
 
     figlet('Strichliste v' + version, function(error, data) {
-        if (!error) {
-            console.log(data);
-        }
+        if (!error) console.log(data);
 
         console.log('running on port ' + configuration.port);
     });
