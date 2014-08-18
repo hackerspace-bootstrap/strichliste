8dfc8c5cc5f026876fbe55f58686cfc568d1b93e
zaphod1984 <zaphod84@gmx.de>
2014-08-18 10:50:43 +0200

    refactors database creation

--- createDatabase.js
+++ createDatabase.js
@@ -1,8 +1,8 @@
 var seq = require('seq');
 var args = require('yargs').argv;
 
-var Factory = require('./lib/database/Factory');
 var config = require('./lib/configuration');
+var createDatabase = require('./lib/util/createDatabase');
 
 var fileName = args.filename || null;
 var dbOptions = config.database;
@@ -11,20 +11,8 @@ if (fileName) {
     dbOptions.options.filename = fileName;
 }
 
-Factory.create(dbOptions, function(error, db) {
+createDatabase(dbOptions, function(error) {
     if (error) return console.log(error.message);
 
-    seq()
-        .par(function() {
-            db.query('CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name text, createDate DATETIME DEFAULT current_timestamp)', [], this);
-        })
-        .par(function() {
-            db.query('CREATE TABLE transactions (id INTEGER PRIMARY KEY AUTOINCREMENT, userId INTEGER, createDate DATETIME DEFAULT current_timestamp, value NUMERIC)', [], this);
-        })
-        .seq(function() {
-            db.query('CREATE INDEX userId ON transactions(userId)', [], this);
-        })
-        .seq(function() {
-            console.log('database ' + dbOptions.options.filename + ' created');
-        });
+    console.log('database ' + dbOptions.options.filename + ' created');
 });
