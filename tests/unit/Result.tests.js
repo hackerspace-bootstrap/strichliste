d4abb0cd808800fe5233ce7ce24b6af9e5a89805
zaphod1984 <zaphod84@gmx.de>
2014-08-20 10:21:08 +0200

    adds sane descriptions

--- tests/unit/Result.tests.js
+++ tests/unit/Result.tests.js
@@ -5,7 +5,7 @@ var List = require('../../lib/List');
 var LimitStatement = require('../../lib/parameters/LimitStatement');
 
 describe('list', function () {
-    it('sss', function () {
+    it('should aggregate with limitstatement', function () {
         var a = new List([1, 2, 3], 23, new LimitStatement(99, 88));
 
         expect(a.toObject()).to.deep.equal({
@@ -16,7 +16,7 @@ describe('list', function () {
         });
     });
 
-    it('sss', function () {
+    it('should aggregate without limitstatement', function () {
         var a = new List([1, 2, 3], 23);
 
         expect(a.toObject()).to.deep.equal({
@@ -29,7 +29,7 @@ describe('list', function () {
 });
 
 describe('result', function () {
-    it('sss', function () {
+    it('should create a result /w plain object', function () {
         var a = new Result({a: 1}, Result.CONTENT_TYPE_JSON, 400);
 
         expect(a.content()).to.deep.equal({a: 1});
@@ -37,7 +37,7 @@ describe('result', function () {
         expect(a.statusCode()).to.equal(400);
     });
 
-    it('bbb', function () {
+    it('should create result /w list', function () {
         var list = new List([1, 2, 3], 23, new LimitStatement(99, 88));
         var a = new Result(list, Result.CONTENT_TYPE_JSON, 400);
 
@@ -49,5 +49,5 @@ describe('result', function () {
         });
         expect(a.contentType()).to.equal('application/json');
         expect(a.statusCode()).to.equal(400);
-    })
+    });
 });
