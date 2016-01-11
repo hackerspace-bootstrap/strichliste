ffdd16c42267513270d60f4d66e944dd32355edf
seriousManual <seriousstuff@gmx.de>
2016-01-11 09:41:46 +0100

    fixes database creation script

--- createDatabase.js
+++ createDatabase.js
@@ -2,6 +2,7 @@ var seq = require('seq');
 var args = require('yargs').argv;
 
 var config = require('./lib/configuration');
+var dbFactory = require('./lib/database/Factory');
 var createDatabase = require('./lib/util/createDatabase');
 
 var fileName = args.filename || null;
@@ -11,8 +12,12 @@ if (fileName) {
     dbOptions.options.filename = fileName;
 }
 
-createDatabase(dbOptions, function(error) {
-    if (error) return console.log(error.message);
+dbFactory.create(dbOptions, function (error, db) {
+    if (error) throw error;
 
-    console.log('database ' + dbOptions.options.filename + ' created/updated');
+    createDatabase(db, function (error) {
+        if (error) throw error;
+
+        console.log('database ' + dbOptions.options.filename + ' created/updated');
+    });
 });
